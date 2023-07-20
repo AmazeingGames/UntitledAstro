809ac618a7e8a28c8cddedbe64237a6f80d28ce3
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-07-20 12:14:23 -0700

    Functional Tunnel Movement
    Reorganized and refactored scripts to reduce excess scripts and created functional rotational movement for the tunnels.

--- Assets/Scripts/SpinTunnel.cs
+++ Assets/Scripts/SpinTunnel.cs
@@ -6,22 +6,41 @@ public class SpinTunnel : MonoBehaviour
 {
     public float speed = 5;
 
+    List<Transform> tunnels = new List<Transform>();
     void Start()
     {
+        int tunnelsToGet = transform.childCount;
 
+        for (int i = 0; i < tunnelsToGet; i++)
+        {
+            tunnels.Add(transform.GetChild(i));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        float input = Input.GetAxisRaw("Horizontal");
+
+        foreach (Transform tunnel in tunnels)
         {
-            this.transform.Rotate(0f, 0f, (speed * Time.deltaTime));
+            tunnel.transform.Rotate(0f, 0f, speed * input * Time.deltaTime);
+
+            Quaternion tunnelRotation = tunnel.transform.rotation;
+
+            Debug.Log(tunnel.transform.rotation);
+
+            Quaternion minimum = new Quaternion(0.00000f, 0.00000f, -0.26234f, 0.96498f);
+
+            Quaternion maximum = new Quaternion(0.00000f, 0.00000f, 0.70452f, 0.70968f);
+
+
+
+
+
+            //Mathf.Clamp(tunnel.transform.rotation, minimum, maximum);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.Rotate(0f, 0f, -(speed * Time.deltaTime));
-        }
+
     }
 }
