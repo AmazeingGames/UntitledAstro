bb35cd6fc69c3390cb8fac7a22d1a8d7c927928b
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-07-20 13:14:22 -0700

    Flip Script Woking
    Tunnels now flip properly, though it's difficult to tell when a flip happens.

--- Assets/Scripts/SpinTunnel.cs
+++ Assets/Scripts/SpinTunnel.cs
@@ -11,11 +11,16 @@ public class SpinTunnel : MonoBehaviour
     [SerializeField] bool shouldStopAtThreshold = true;
     [SerializeField] float minimumStopThreshold = .5f;
 
-    readonly Quaternion minimum = new Quaternion(0.00000f, 0.00000f, -0.26234f, 0.96498f);
+    bool isOnBottom = true;
 
-    readonly Quaternion maximum = new Quaternion(0.00000f, 0.00000f, 0.70452f, 0.70968f);
+    readonly Quaternion bottomMinimum = new Quaternion(0.00000f, 0.00000f, -0.26234f, 0.96498f); //(-30)
+    readonly Quaternion bottomMaximum = new Quaternion(0.00000f, 0.00000f, 0.70452f, 0.70968f); //(90)
 
-    List<Transform> tunnels = new List<Transform>();
+    readonly Quaternion topMinimum = new Quaternion(0.00000f, 0.00000f, 0.96498f, 0.26234f); //(150)
+    readonly Quaternion topMaximum = new Quaternion(0.00000f, 0.00000f, -0.70968f, 0.70452f); //(-90)
+
+    readonly List<Transform> tunnels = new List<Transform>();
+
     void Start()
     {
         int tunnelsToGet = transform.childCount;
@@ -29,16 +34,60 @@ public class SpinTunnel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateTunnels();
+
+        FlipTunnels();
+    }
+
+    void FlipTunnels()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            isOnBottom = !isOnBottom;
+
+            foreach (Transform tunnel in tunnels)
+            {
+                //Flips all the tunnels by 180 degrees
+                tunnel.rotation *= Quaternion.AngleAxis(180, Vector3.forward);
+
+            }
+        }
+    }
+
+    //Rotates all the tunnels according to the horizontal axes keys (left, right)
+    void RotateTunnels()
+    {
         float input = Input.GetAxisRaw("Horizontal");
 
         foreach (Transform tunnel in tunnels)
         {
+            Debug.Log(tunnel.rotation);
+
             float amountToRotate = speed * input * Time.deltaTime;
+
+            /* This code looks more complicated than it really is.
+             * What this does is if the given value (isOnBottom) is true, the value we're trying to assign becomes the value after the ? operator (bottomMaximum), else it becomes the value after the : operator (topMaximum)
+             * It's essentially the same as:
+             * 
+             * if (isOnBottom == true)
+             * {
+             *    minimumToUse = bottomMinimum;
+             *    maximumToUse = bottomMaximum;
+             * }
+             * else
+             * {
+             *   minimumToUse = topMinimum;
+             *     maximumToUse = topMaximum;
+             * }
+            */
+
+            Quaternion minimumToUse = isOnBottom ? bottomMinimum : topMinimum;
+            Quaternion maximumToUse = isOnBottom ? bottomMaximum : topMaximum;
 
             //This code determines the angle of the tunnels and the tunnels stopping point, and returns an angle of how close they match.
             //The closer to 0, the closer the two angles are.
-            float differenceInMinimumAngle = Quaternion.Angle(tunnel.rotation, minimum);
-            float differenceInMaximumAngle = Quaternion.Angle(tunnel.rotation, maximum);
+            float differenceInMinimumAngle = Quaternion.Angle(tunnel.rotation, minimumToUse);
+            float differenceInMaximumAngle = Quaternion.Angle(tunnel.rotation, maximumToUse);
 
             //Only rotates if we're not close the rotational bounds
             if (shouldStopAtThreshold && ((amountToRotate < 0 && differenceInMinimumAngle < minimumStopThreshold) || (amountToRotate > 0 && differenceInMaximumAngle < minimumStopThreshold)))
@@ -47,10 +96,6 @@ public class SpinTunnel : MonoBehaviour
                 break;
             }
             tunnel.transform.Rotate(0f, 0f, amountToRotate);
-
-            //Mathf.Clamp(tunnel.transform.rotation, minimum, maximum);
         }
-
-
     }
 }
