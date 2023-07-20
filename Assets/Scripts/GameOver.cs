47415868ab4839038d6581bbe900446436c923e7
Flare <jntnnn4@gmail.com>
2023-07-20 12:04:44 -0700

    game over script, spawn manager script

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private bool gameOver = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject asteroidPrefab;
+    private Vector3 spawnPos = new Vector3(0, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void SpawnObstacle()
+    {
+        Instantiate(asteroidPrefab, spawnPos, asteroidPrefab.transform.rotation);
+    }
+
 }
