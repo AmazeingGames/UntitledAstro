ec0e45ca633b0bc28e1634d56848aefa35527e3c
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-07-22 13:16:48 -0700

    Got Tunnel Spin, Flip, & Threshold Working
    Fixed the issues with the tunnel flip, tunnel spin, and tunnel threshold. Every script and feature works properly, with the minor exception that the tunnels move slightly when rotating. However, this is is an easy fix.

--- Assets/Scripts/SpinTunnel.cs
+++ Assets/Scripts/SpinTunnel.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Windows;
 using static UnityEngine.Rendering.DebugUI;
+using Input = UnityEngine.Input;
 
 public class SpinTunnel : MonoBehaviour
 {
@@ -13,13 +16,15 @@ public class SpinTunnel : MonoBehaviour
     [SerializeField] bool shouldStopAtThreshold = true;
     [SerializeField] float minimumStopThreshold = .5f;
 
+    [SerializeField] Transform botPanel;
+    [SerializeField] Transform topPanel;
     bool isOnBottom = true;
 
-    readonly Quaternion bottomMinimum = new Quaternion(0.00000f, 0.00000f, -0.26234f, 0.96498f); //(-30)
-    readonly Quaternion bottomMaximum = new Quaternion(0.00000f, 0.00000f, 0.70452f, 0.70968f); //(90)
+    readonly Quaternion bottomMinimum = new Quaternion(0.00000f, 0.00000f, 0.96498f, 0.26234f); //(150)
+    readonly Quaternion bottomMaximum = new Quaternion(0.00000f, 0.00000f, 0.70968f, -0.70452f); //(90)
 
     readonly Quaternion topMinimum = new Quaternion(0.00000f, 0.00000f, 0.96498f, 0.26234f); //(150)
-    readonly Quaternion topMaximum = new Quaternion(0.00000f, 0.00000f, -0.70968f, 0.70452f); //(-90)
+    readonly Quaternion topMaximum = new Quaternion(0.00000f, 0.00000f, -0.70968f, 0.70452f); //(270)
 
     readonly List<Transform> tunnels = new List<Transform>();
     readonly List<List<Transform>> listOfPanels = new();
@@ -69,30 +74,59 @@ public class SpinTunnel : MonoBehaviour
 
             Debug.Log("We Flipped!");
 
-            foreach (Transform tunnel in tunnels)
+            for (int i = 0; i < listOfPanels.Count; i++)
             {
-                //Flips all the tunnels by 180 degrees
-                tunnel.rotation *= Quaternion.AngleAxis(180, Vector3.forward);
+                for (int n = 0; n < listOfPanels[i].Count; n++)
+                {
+                    //Flips all the tunnels by 180 degrees
+                    var panel = listOfPanels[i][n];
+                    panel.rotation *= Quaternion.AngleAxis(180, Vector3.forward);
+                }
             }
         }
-    }
+    } 
 
     //Rotates all the tunnels according to the horizontal axes keys (left, right)
     void RotateTunnels()
     {
+        Debug.Log("Bot Panel: " + botPanel.rotation);
+        Debug.Log("Top Panel: " + topPanel.rotation);
+        
+        float input = Input.GetAxisRaw("Horizontal");
+        float amountToRotate = speed * input * Time.deltaTime;
+
+        if (ShouldStopRotating(amountToRotate))
+            return;
 
         for (int i = 0; i < listOfPanels.Count; i++)
         {
             for (int n = 0; n < listOfPanels[i].Count; n++)
             {
-                float input = Input.GetAxisRaw("Horizontal");
 
                 var currentList = listOfPanels[i];
                 var panel = currentList[n];
 
-                float amountToRotate = speed * input * Time.deltaTime;
                 panel.Rotate(0f, 0f, amountToRotate);
             }
         }
+    }
+
+    bool ShouldStopRotating(float amountToRotate)
+    {
+        Quaternion minimumToUse = isOnBottom ? bottomMinimum : topMinimum;
+        Quaternion maximumToUse = isOnBottom ? bottomMaximum : topMaximum;
+
+        Transform panelToUse = isOnBottom ? botPanel : topPanel;
+
+        float differenceInMinimumAngle = Quaternion.Angle(panelToUse.rotation, minimumToUse);
+        float differenceInMaximumAngle = Quaternion.Angle(panelToUse.rotation, maximumToUse);
+
+        //Returns true if we're too close to the rotational bounds
+        if (shouldStopAtThreshold && ((amountToRotate < 0 && differenceInMinimumAngle < minimumStopThreshold) || (amountToRotate > 0 && differenceInMaximumAngle < minimumStopThreshold)))
+        {
+            Debug.Log("We are too close to the edge; can't move any farther");
+            return true;
+        }
+        return false;
     }
 }
