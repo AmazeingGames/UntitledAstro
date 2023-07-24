278e8843d1255e8e4645bb05a0bc24fec873f3df
Flare <jntnnn4@gmail.com>
2023-07-23 23:42:35 -0700

    tunneling

--- Assets/Scripts/Tunnel Scripts/RepeatTunnel.cs
+++ Assets/Scripts/Tunnel Scripts/RepeatTunnel.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RepeatTunnel : MonoBehaviour
 {
     public float speed = 5;
-    private Vector3 startPos;
+    public float behindplayer = 14;
+    public float teleporttoZ;
+    List<Transform> tunnels = new List<Transform>();
+    private Transform lastTunnel;
+    public bool shouldmovetunnel = true;
     void Start()
     {
-        startPos = transform.position;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform tunnel = transform.GetChild(i);
+            tunnels.Add(tunnel);
+        }
     }
 
     // Update is called once per frame
@@ -16,9 +26,26 @@ public class RepeatTunnel : MonoBehaviour
     {
         transform.Translate(speed * Time.deltaTime * Vector3.forward);
 
-        if (transform.position.z > startPos.z + 5)
+        for (int i = 0; i < tunnels.Count; i++)
         {
-            transform.position = startPos;
+            Transform teleport = tunnels[i];
+            if (teleport.transform.position.z >= behindplayer)
+            {
+                float smallestZ = tunnels[0].transform.position.z;
+                for (int n = 0; n < tunnels.Count; n++)
+                {
+                    if (smallestZ > tunnels[n].transform.position.z)
+                    {
+                        smallestZ = tunnels[n].transform.position.z;
+                        lastTunnel = tunnels[n];
+                    }
+                }
+
+                float lengthofpanel = teleport.GetChild(0).GetComponent<Collider>().bounds.size.z;
+
+                tunnels[i].transform.position = new Vector3(0, 0, smallestZ - lengthofpanel);
+                    
+            }
         }
     }
 }
