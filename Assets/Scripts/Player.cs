63387e492685380bc8bc92916c0ae86fdc94783a
Flare <jntnnn4@gmail.com>
2023-08-02 19:34:21 -0700

    fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,26 @@ public class Player : MonoBehaviour
 {
     public ParticleSystem explosionParticle1;
     public ParticleSystem explosionParticle2;
+    private GameOver gameOver;
+    public GameObject gameManager;
+    public GameObject playerModel;
     // Start is called before the first frame update
     void Start()
-    { 
-    
-    
+    {
+        gameManager = GameObject.Find("GameManager");
+        gameOver = gameManager.GetComponent<GameOver>();
+        gameOver.GameOverEvent += ExplodeShip;
     }
     // Update is called once per frame
     void Update()
     {
         
     }
-
-    private void OnTriggerEnter(Collider other)
+    private void ExplodeShip()
     {
-        Destroy(gameObject);
+        playerModel.SetActive(false);
         explosionParticle1.Play();
         explosionParticle2.Play();
     }
+
 }
